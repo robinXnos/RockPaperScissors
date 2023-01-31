ef16442e463963b966965888a125e3cafc896783
robinXnos <122937759+robinXnos@users.noreply.github.com>
2023-01-31 22:37:21 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 
@@ -14,22 +13,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // scanner object
         String exit = "yes"; // useless
-        int counter = 1;
+        int round = 1;
 
         while (!exit.equalsIgnoreCase("nein")){   // loop until player wants to exit
-            System.out.println(ANSI_YELLOW + "|| Runde " + counter + ". ||" + ANSI_RESET); // fancy thing to express the round number
+            System.out.println(ANSI_YELLOW + "|| Runde " + round + ". ||" + ANSI_RESET); // fancy thing to express the round number
             System.out.println(ANSI_PURPLE + "Spieler 1" + ANSI_RESET + " ist dran!"); // output demand
-            String one = scanner.next(); // saves the input of player 1
+            String player1 = scanner.next(); // saves the input of player 1
             System.out.println(ANSI_BLUE + "Spieler 2" + ANSI_RESET + " ist dran!"); // output demand
-            String two = scanner.next(); // saves the input of player 2
+            String player2 = scanner.next(); // saves the input of player 2
 
-            Game game = new Game(one, two); // game object for OOP ;)
+            Game game = new Game(player1, player2); // game object for OOP ;)
             game.checkwin(); // method to compare both inputs
             System.out.println(ANSI_RED + "\nWillst du nochmal spielen? JA ODER NEIN ?!!?! ... ANTWORTE DOCH MAN" + ANSI_RESET); // output question
             exit = scanner.next(); // every input except "no" gets ignored --> while-loop
-
-            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n"); // "clear the board"
-            counter ++;
+            for (int i = 0; i < 30; i++)
+                System.out.println();       // clear the board
+            round ++;
         }
     }
 }
