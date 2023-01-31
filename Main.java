b5b1f2d5827c253d5eb15250f71ed02c3c768588
robinXnos <122937759+robinXnos@users.noreply.github.com>
2023-01-31 21:59:26 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,35 @@
 package com.company;
 
+import java.io.IOException;
 import java.util.Scanner;
 
+
 public class Main {
+    public static final String ANSI_RED = "\u001B[31m";     // color for output
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_RESET = "\u001B[0m";
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String exit = "yes";
-        while (!exit.equalsIgnoreCase("no")){
-            System.out.println("Player 1, its your turn!");
-            String one = scanner.next();
-            System.out.println("Player 2, its your turn!");
-            String two = scanner.next();
+        Scanner scanner = new Scanner(System.in); // scanner object
+        String exit = "yes"; // useless
+        int counter = 1;
 
-            Game game = new Game(one, two);
-            game.checkwin();
-            System.out.println("another game? yes or no???? ANSWER ME !!!!");
-            exit = scanner.next();
+        while (!exit.equalsIgnoreCase("nein")){   // loop until player wants to exit
+            System.out.println(ANSI_YELLOW + "|| Runde " + counter + ". ||" + ANSI_RESET); // fancy thing to express the round number
+            System.out.println(ANSI_PURPLE + "Spieler 1" + ANSI_RESET + " ist dran!"); // output demand
+            String one = scanner.next(); // saves the input of player 1
+            System.out.println(ANSI_BLUE + "Spieler 2" + ANSI_RESET + " ist dran!"); // output demand
+            String two = scanner.next(); // saves the input of player 2
+
+            Game game = new Game(one, two); // game object for OOP ;)
+            game.checkwin(); // method to compare both inputs
+            System.out.println(ANSI_RED + "\nWillst du nochmal spielen? JA ODER NEIN ?!!?! ... ANTWORTE DOCH MAN" + ANSI_RESET); // output question
+            exit = scanner.next(); // every input except "no" gets ignored --> while-loop
+
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n"); // "clear the board"
+            counter ++;
         }
     }
 }
